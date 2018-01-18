587b03aef6cdceb53b128f216ad5b9a5b0db012f
Chris Hernandez <chris@christopherahernandez.com>
2018-01-17 22:38:33 -0500

    Menu clean up

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,6 +1,29 @@
 #include "menu.h"
+#include <QWidget>
+#include <QMouseEvent>
 
-Menu::Menu(QWidget *parent) : QWidget(parent)
-{
+Menu::Menu(QWidget *parent) : QWidget(parent){
+    this->parent = parent;
+}
 
+void Menu::mousePressEvent(QMouseEvent *e){
+    dragging = true;
+    previous_pos = e->globalPos();
+}
+
+void Menu::mouseMoveEvent(QMouseEvent *e){
+    if(dragging){
+        // Calculate the difference between the window's last position and where the mouse is now
+        QPoint delta = e->globalPos() - previous_pos;
+
+        // Move the window to that position
+        this->window()->move(this->window()->x() + delta.x(), this->window()->y() + delta.y());
+
+        // Set the window's new position
+        previous_pos = e->globalPos();
+    }
+}
+
+void Menu::mouseReleaseEvent(QMouseEvent *e){
+    dragging = false;
 }
